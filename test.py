be1e1177d82109a9a7d6e1692bb4e79ec70fe356
Chris Lam <chris.lam@oracle.com>
2016-04-03 13:05:43 -0500

    added some test to figure out how to read a yanl file :

--- test.py
+++ test.py
@@ -1,39 +1,68 @@
 import requests
 import json
-import status_check
 import os
+import yaml
 
 def create_url():
     config_dir = os.path.dirname(__file__)
-    print(config_dir)
-    rel_path = 'config.txt'
-    print(rel_path)
+    rel_path = 'config.yml'
     abs_file_path = os.path.join(config_dir, rel_path)
-    print(abs_file_path)
 
-    with open(abs_file_path) as f:
-        for dns in f:
-            url = 'https://{0}/services/search/duvet.do'.format(dns.rstrip())
+    with open(abs_file_path, 'r') as ymlfile:
+        cfg = yaml.load(ymlfile)
 
-    return url
+    print('-----------------------------')
+    #print(cfg)
+    print('-----------------------------')
 
-def get_requests_github():
-    r = requests.get('https://api.github.com/events')
-    return r
+    #for section in cfg:
+    #    print(section['config'])
+
+    print('------ config-------')
+    configs = cfg[0]
+    print(configs)
+    print('------- common ---------------')
+    common_setting = cfg[1]
+    print(common_setting)
+    print('-------- test -----------')
+    tests = cfg[2]
+    print(tests)
+    print(type(tests))
+    print('-----------  get tests  -----------')
+    test_values = tests['tests']
+    print(test_values)
+    print('-----------  iterate tests  -----------')
+    # iterate the test in tests yaml file
+    for individual_test in test_values:
+        # retrieves the individual test yamls in the tests block
+        test = individual_test['test']
+        # iterate the attributes of the test yaml
+        for params in test:
+            key = params.keys()[0]
+            if key == 'name':
+                print('---------------')
+            #print(key, params[key])
+            print(params[key])
+
+
+
+            #url = 'https://{0}/services/search/duvet.do'.format(dns.rstrip())
+
+    return ''
+
 
 def get_requests_ocp_search():
+
     url = create_url()
-    headers = {'app-id': '4894f63625ed4dfc809b11ac42c2ae8b', 'Services-Source-Type': 'SDSS-Web',
-               'Accept': 'application/json'}
-    r = requests.get(url, verify=False, headers=headers)
-    return r
+#    headers = {'app-id': '4894f63625ed4dfc809b11ac42c2ae8b', 'Services-Source-Type': 'SDSS-Web', 'Accept': 'application/json'}
+#    r = requests.get(url, verify=False, headers=headers)
+#    return r
 
 resp = get_requests_ocp_search()
 
 #status code
-status_check.check_statuses(resp)
 
 #pretty print
-Eparsed = json.dumps(resp.json(), indent=4)
+#parsed = json.dumps(resp.json(), indent=4)
 #print parsed
 
