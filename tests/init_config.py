c3af57eaf50635b748697caacb4de9479b46afef
Chris Lam <chris.lam@oracle.com>
2016-04-06 11:26:23 -0500

    got basic validation additional validation working

--- tests/init_config.py
+++ tests/init_config.py
@@ -4,84 +4,85 @@ import collections
 from rest_get import RestTestGet
 
 def _open_and_load_yaml_file():
-    _config_dir = os.path.dirname(__file__)
-    _rel_path = 'config.yml'
-    _abs_file_path = os.path.join(_config_dir, _rel_path)
+    config_dir = os.path.dirname(__file__)
+    rel_path = 'config.yml'
+    abs_file_path = os.path.join(config_dir, rel_path)
 
-    with open(_abs_file_path, 'r') as ymlfile:
-        _cfg = yaml.load(ymlfile)
+    with open(abs_file_path, 'r') as ymlfile:
+        cfg = yaml.load(ymlfile)
 
-    return _cfg
+    return cfg
 
 def _get_host_name(cfg):
-    _configs = cfg[0]
-    _config_list = _configs['config']
-    _hostname = None
-    for c in _config_list:
-        _hostname = c['hostname']
+    configs = cfg[0]
+    config_list = configs['config']
+    hostname = ''
+    for c in config_list:
+        hostname = c['hostname']
 
-    return _hostname
+    return hostname
 
 def _get_headers(cfg):
-    _common_settings = cfg[1]
-    _headers = None
-    for common_setting in _common_settings:
-        _cs = _common_settings[common_setting]
-        for c in _cs:
-            _headers = c['headers']
+    common_settings = cfg[1]
+    headers = ''
+    for common_setting in common_settings:
+        cs = common_settings[common_setting]
+        for c in cs:
+            headers = c['headers']
 
-    return _headers
+    return headers
 
 def _get_test_attributes(cfg):
     TestAttributes = collections.namedtuple('TestAttributes', ['name', 'url', 'method',
                                                                'comparisons'])
 
-    _tests = cfg[2]
-    _test_values = _tests['tests']
-    _test_attributes = list()
+    tests = cfg[2]
+    test_values = tests['tests']
+    test_attributes = list()
 
     # iterate the test in tests yaml file
-    for individual_test in _test_values:
+    for individual_test in test_values:
         # retrieves the individual test yamls in the tests block
-        _test = individual_test['test']
+        test = individual_test['test']
         # iterate the attributes of the test yaml
-        _name = None
-        _url = None
-        _method = None
-        _comparisons = None
+        name = None
+        url = None
+        method = None
+        comparisons = None
 
-        for params in _test:
-            _key = params.keys()[0]
-            if _key == 'name':
-                _name = params[_key]
+        for params in test:
+            key = params.keys()[0]
+            if key == 'name':
+                name = params[key]
 
-            if _key == 'url':
-                _url = params[_key]
+            if key == 'url':
+                url = params[key]
 
-            if _key == 'method':
-                _method = params[_key]
+            if key == 'method':
+                method = params[key]
 
-            if _key == 'validations':
-                _validations = params[_key]
-                _comparisons = _get_comparisons(_validations)
+            if key == 'validations':
+                validations = params[key]
+                comparisons = _get_comparisons(validations)
 
-        _test_attribute = TestAttributes(_name, _url, _method, _comparisons)
-        _test_attributes.append(_test_attribute)
 
-    return _test_attributes
+        test_attribute = TestAttributes(name, url, method, comparisons)
+        test_attributes.append(test_attribute)
+
+    return test_attributes
 
 def _get_comparisons(validations):
-    _comparisons = list()
+    comparisons = list()
     for comparison in validations:
-        _comparisons.append(comparison)
+        comparisons.append(comparison)
 
-    return _comparisons
+    return comparisons
 
 def _generate_url(hostname, attributes):
-    _url = 'https://{0}{1}'
-    _url = _url.format(hostname, attributes.url)
+    url = 'https://{0}{1}'
+    url = url.format(hostname, attributes.url)
 
-    return _url
+    return url
 
 def init():
     _cfg = _open_and_load_yaml_file()
@@ -95,13 +96,15 @@ def init():
 
     for a in _attributes:
         _restful_url = _generate_url(_hostname, a)
+        #print(_restful_url)
         # when we do the POST we want to create a factory
-        _rest_get = RestTestGet(_restful_url, _headers, a.comparisons)
+        rest_get = RestTestGet(_restful_url, _headers, a.comparisons)
 
         #for c in a.comparisons:
         #    print('chris', c)
 
-        _response = _rest_get.test_web_service()
-        print(_response)
+        response = rest_get.test_web_service()
+        #response = rest_get.get_response()
+        print(response)
 
 init()
--- tests/json_verification.py
+++ tests/json_verification.py
@@ -3,11 +3,10 @@ import collections
 
 class JsonVerification:
 
-    _json_messages_from_get_all = None
-
     def __init__(self, response, comparisons):
-        self.response = response
-        self.comparisons = comparisons
+        self._response = response
+        self._comparisons = comparisons
+        self._json_messages_from_get_all_matching = None
 
     def _retrieve_key_from_comparison(self, comparison):
         ComparisonAttributes = collections.namedtuple('ComparisonAttributes', ['key', 'value',
@@ -17,42 +16,49 @@ class JsonVerification:
         #values, etc
         for attribute in comparison:
             if attribute != 'expected' and attribute != 'comparator':
-                print('_retrieved key is:', attribute)
-                print('_retrieved key value is:', comparison[attribute])
-                print('_retrieved expected value:', comparison['expected'])
-                print('_retrieved comparator value:', comparison['comparator'])
+                #print('_retrieved key is:', attribute)
+                #print('_retrieved key value is:', comparison[attribute])
+                #print('_retrieved expected value:', comparison['expected'])
+                #print('_retrieved comparator value:', comparison['comparator'])
                 _comparison_attributes = ComparisonAttributes(attribute, comparison[attribute],
                                                               comparison['expected'],
                                                               comparison['comparator'])
+                return _comparison_attributes
 
-    def _get_all(self, myjson, key):
+    def _get_all_matching(self, myjson, key):
         if type(myjson) == str:
             myjson = json.loads(myjson)
         if type(myjson) is dict:
             for jsonkey in myjson:
                 if type(myjson[jsonkey]) in (list, dict):
                     if jsonkey == key:
-                        #print('in _get_all function')
+                        #print('in _get_all_matching function')
                         #print(str(myjson[jsonkey]))
-                        self._json_messages_from_get_all.append(myjson[jsonkey])
-                    self._get_all(myjson[jsonkey], key)
+                        self._json_messages_from_get_all_matching.append(myjson[jsonkey])
+                    self._get_all_matching(myjson[jsonkey], key)
         elif type(myjson) is list:
             for item in myjson:
                 if type(item) in (list, dict):
-                    self._get_all(item, key)
+                    self._get_all_matching(item, key)
 
-    def _validate_each_comparison(self, comparison, list_of_json):
-        print("comparison to use:", comparison)
+    def _validate_each_comparison(self, comparison_attribute, list_of_json):
+        _attribute_value = comparison_attribute.value
+        _expected = comparison_attribute.expected
+
         for json in list_of_json:
-            print('_validate_each_comparison:', json)
+            if str(json[_attribute_value]) != _expected:
+                print('Comparison failed, the actual value expected does not match the expected')
+                print('value', str(json[_attribute_value]))
+                print('expected', _expected)
+            else:
+                print("value matches expected")
 
     def validate(self):
-        for comparison in self.comparisons:
+        for comparison in self._comparisons:
             comparison_value = comparison['compare']
-            self._json_messages_from_get_all = list()
-            json_messages_from_get_all  = list()
-            key = self._retrieve_key_from_comparison(comparison_value)
-            self._get_all(self.response, 'messages')
-            self._validate_each_comparison(comparison_value, self._json_messages_from_get_all)
-
+            self._json_messages_from_get_all_matching = list()
+            comparison_attribute = self._retrieve_key_from_comparison(comparison_value)
+            key = comparison_attribute.key
+            self._get_all_matching(self._response, key)
+            self._validate_each_comparison(comparison_attribute, self._json_messages_from_get_all_matching)
 
